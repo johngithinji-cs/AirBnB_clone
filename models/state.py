d3b3b8d7e133307c1a862cd5ee51391f9146aef6
kingsleyocran <ocrankingsley@gmail.com>
2022-08-07 15:45:36 +0000

    pycodestyle fixes

--- models/state.py
+++ models/state.py
@@ -2,9 +2,9 @@
 """Define the State class."""
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
     """Represent a state.
-
     Attributes:
         name (str): The name of the state.
     """
--- models/user.py
+++ models/user.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """Introduces the User class"""
-from models.base_model import Basemodel
-
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """Models a User.
-    
-    Attributes:
-        
-    email: string - empty string
-    password: string - empty string
-    first_name: string - empty string
-    last_name: string - empty string
+    - Attributes:
+    - email: string - empty string
+    - password: string - empty string
+    - first_name: string - empty string
+    - last_name: string - empty string
     """
     email = ""
     password = ""
